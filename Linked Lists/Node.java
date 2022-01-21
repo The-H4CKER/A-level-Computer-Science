dae17c6c238be29d763da13ebf28fc3a0a5b9d6d
Nigel Jacob <66517600+The-H4CKER@users.noreply.github.com>
2022-01-21 23:23:09 +0000

    Update Node.java

--- Linked Lists/Node.java
+++ Linked Lists/Node.java
@@ -1,51 +1,51 @@
 package lists;
 
 public class Node {
-	
 	int dataField;
 	Node prvNode;
 	Node nxtNode;
-	
+
 	Node(int dataField, Node prvNode, Node nxtNode) {
 		this.dataField = dataField;
 		this.prvNode = prvNode;
 		this.nxtNode = nxtNode;
 	}
-		
-	int getDataField() {
-		return this.dataField;
-	}
-	
-	Node getPrvNode() {
-		return this.prvNode;
-	}
-	
-	Node getNxtNode() {
-		return this.nxtNode;
-	}
-	
-	void setDataField(int dataField) {
+
+	Node(int dataField) {
 		this.dataField = dataField;
 	}
-	
-	void setPrvNode(Node node) {
-		this.prvNode = node;
+
+	Node GetNxtNode() {
+		return nxtNode;
 	}
-	
-	void setNxtNode(Node node) {
-		this.nxtNode = node;
+
+	Node GetPrvNode() {
+		return prvNode;
 	}
-	
-	
+
+	int GetDataField() {
+		return dataField;
+
+	}
+
+	void SetNxtNode(Node nxtNode) {
+		this.nxtNode = nxtNode;
+	}
+
+	void SetPrvNode(Node prvNode) {
+		this.prvNode = prvNode;
+	}
+
+	void SetDataField(int dataField) {
+		this.dataField = dataField;
+	}
+
 	public String toString() {
-		String string = "Data Field: " + this.dataField + "\n";
-		if (this.prvNode != null) {
-			string += "Previous Node: " + this.prvNode.dataField + "\n";
-		}
-		if (this.nxtNode != null) {
-			string += "Next Node: " + this.nxtNode.dataField + "\n";
-		}
-		return string;
+		String txt = "\nData Field: \t" + dataField;
+		if (prvNode != null)
+			txt += "\n" + "Previous node:\t" + prvNode.dataField;
+		if (nxtNode != null)
+			txt += "\n" + "Previous node:\t" + nxtNode.dataField;
+		return txt;
 	}
-	
 }
